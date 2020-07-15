5a8aff4dbdb4cddca298cf06e81a42d05041e48b
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-15 23:35:31 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,7 +1,25 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-
+coupons.each do |coupon|
+   item_info = find_item_by_name_in_collection(coupon[:item], cart)
+  	  item_w_coupon = find_item_by_name_in_collection(coupon[:item]+" W/COUPON", cart)
+end	   
+if item_w_coupon and item_info[:count] >= coupon[:num]
+	    item_w_coupon[:count] += coupon[:num]
+	    item_info[:count] -= coupon[:num]
+	  elsif item_info and item_info[:count] >= coupon[:num]
+      cart << {
+        :item => coupon[:item] + " W/COUPON",
+        :price => (coupon[:cost]/coupon[:num]).round(2),
+        :clearance => item_info[:clearance],
+        :count => coupon[:num]
+      }
+      item_info[:count] -= coupon[:num]
+    end 
+  end 
+  cart.delete_if{|item_info| item_info[:count] <= 0}
+  cart
  
 end
 
