fcc6d9f6cb9cdd963d961bcda0969b6e33243b41
Margarita Morozova <ritamorozova@yahoo.com>
2020-07-15 23:33:26 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,8 +1,24 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
- new_array =[]
- 
+
+ coupons.each do |coupon| 
+    coupon.each do |attribute, value| 
+      name = coupon[:item] 
+    
+      if cart[name] && cart[name][:count] >= coupon[:num] 
+        if cart["#{name} W/COUPON"] 
+          cart["#{name} W/COUPON"][:count] += 1 
+        else 
+          cart["#{name} W/COUPON"] = {:price => coupon[:cost], 
+          :clearance => cart[name][:clearance], :count => 1} 
+        end 
+  
+      cart[name][:count] -= coupon[:num] 
+    end 
+  end 
+end 
+  cart 
  
 end
 
